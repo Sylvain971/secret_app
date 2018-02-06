80c48965a39e88323ea1ebeeaae2bd6d350918cc
Sylvain Beaubois <sylvain_beaubois@hotmail.fr>
2018-02-06 07:50:45 +0100

    effort ultime

--- Gemfile
+++ Gemfile
@@ -5,6 +5,8 @@ source 'https://rubygems.org'
 #gem 'pundit'
 
 gem 'rails'
+gem 'bootstrap-sass', '3.3.7'
+gem 'bcrypt',         '3.1.11'
 gem 'puma'
 gem 'sass-rails'
 gem 'uglifier'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+	
+	user = User.create(name: "Jean-Claude Vandamme", email: "jeanclaude@lacacahuete.com", password: "cacahuete")
